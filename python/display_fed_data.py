f5f01b0eb6a543715be43dc903c0591290afc4c7
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-09-22 13:13:27 -0500

    Move refresh button and date, time to same row; still working on formatting.

--- python/display_fed_data.py
+++ python/display_fed_data.py
@@ -8,6 +8,7 @@ from flask import Flask, render_template
 # - Load live FED data in json format using requests library
 # - Only put FED status logic in python (not html): save as a new variable
 # - Sort by any column (specified by user) 
+# - Make Refresh button smaller; move to same row as date and time.
 
 # DONE
 # - Get FED status based on multiple variables
